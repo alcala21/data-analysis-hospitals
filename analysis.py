717714c98e0e102dfa84f71831e3b5b2ecb6b34d
Carlos Alcala <alcala21@gmail.com>
2021-12-03 16:58:50 -0600

    Stage 2/5. Data Analysis for Hospitals.

--- analysis.py
+++ analysis.py
@@ -1,9 +1,12 @@
-# write your code here
 import pandas as pd
-
 pd.set_option('display.max_columns', 8)
 
-names = ["general", "prenatal", "sports"]
-dfs = {name: pd.read_csv(f"test/{name}.csv") for name in names}
-for name in names:
-    print(dfs[name].head(20))
+dfs = dict()
+for name in ["general", "prenatal", "sports"]:
+    dfs[name] = pd.read_csv(f"test/{name}.csv")
+    dfs[name].drop("Unnamed: 0",axis=1, inplace=True)
+    dfs[name].rename(str.lower, axis='columns', inplace=True)
+    dfs[name].columns.values[1] = 'gender'
+
+df = pd.concat(dfs, axis=0, ignore_index=True)
+print(df.sample(n=20, random_state=30))
