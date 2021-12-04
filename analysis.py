e88a2df2c3e4e1f50cbbb86e1715c89a9ca65236
Carlos Alcala <alcala21@gmail.com>
2021-12-04 17:41:03 -0600

    Stage 4/5. Data Analysis for Hospitals.

--- analysis.py
+++ analysis.py
@@ -14,5 +14,25 @@ df = (pd.concat(dfs, axis=0, ignore_index=True)
     .replace(['female', 'woman'], 'f')
     .fillna({'gender': 'f'})
     .fillna({'bmi': 0, 'diagnosis': 0, 'blood_test': 0, 'ecg': 0, 'ultrasound': 0, 'mri': 0, 'xray': 0, 'children': 0, 'months': 0}))
-print(df.shape)
-print(df.sample(n=20, random_state=30))
+
+# 1
+n_hospitals = df.hospital.value_counts()
+print(f"The answer to the 1st question is {n_hospitals.idxmax()}")
+
+# 2
+r_stomach = df[df.hospital == 'general'].diagnosis.value_counts()['stomach'] / n_hospitals['general']
+print(f"The answer to the 2nd question is {round(r_stomach, 3)}")
+
+# 3
+r_disc = df[df.hospital == 'sports'].diagnosis.value_counts()['dislocation'] / n_hospitals['sports']
+print(f"The answer to the 3rd question is {round(r_disc, 3)}")
+
+# 4
+df_wide = df.pivot_table(columns='hospital', aggfunc='median')
+diff = df_wide.loc['age', 'general'] - df_wide.loc['age', 'sports']
+print(f"The answer to the 4th question is {diff}")
+
+# 5
+tvals = df.loc[df.blood_test == 't'].hospital.value_counts()
+print(f"The answer to the 5th question is {tvals.idxmax()}, {tvals.max()} blood tests")
+
